113512e95212cb9afe67b63f5dc2cc14382dca9b
KUNAL CHOURE <chourekunal4@gmail.com>
2024-09-13 00:32:02 +0530

    updated

--- Frontend/src/landing_page/home/Stats.jsx
+++ Frontend/src/landing_page/home/Stats.jsx
@@ -15,26 +15,23 @@ export default function Stats () {
                </p>
           </div>
           <div className="info ">
-            <h2>Customer-first always</h2>
-               <p>That is why 1.3+ crore customers Trust
-                Zerodha with 3.5+lakh crores worth of equity investments
+            <h2>No spam or gimmicks</h2>
+               <p>No gimmicks, spam, "gamification", or annoying push notifications. High quality apps that you use at your pace, the way you like.
                </p>
           </div>
           <div className="info ">
-            <h2>Customer-first always</h2>
-               <p>That is why 1.3+ crore customers Trust
-                Zerodha with 3.5+lakh crores worth of equity investments
+            <h2>The Zerodha universe</h2>
+               <p>Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs.
                </p>
           </div>
           <div className="info ">
-            <h2>Customer-first always</h2>
-               <p>That is why 1.3+ crore customers Trust
-                Zerodha with 3.5+lakh crores worth of equity investments
+            <h2>Do better with money</h2>
+               <p>With initiatives like Nudge and Kill Switch, we don't just facilitate transactions, but actively help you do better with your money.
                </p>
           </div>
       </div>
-      <div className="col-6">
-
+      <div className="col-6 mt-5">
+ <img src="media/images/ecosystem.png" alt="" srcset="" style={{width:"100%"}}/>
       </div>
     </div>
    </div>
