15c809f07bb535bb92dcf48cbf2d6fefef432171
KUNAL CHOURE <chourekunal4@gmail.com>
2024-09-19 22:03:54 +0530

    Education updated

--- Frontend/src/landing_page/home/Pricing.jsx
+++ Frontend/src/landing_page/home/Pricing.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 
 export default function Pricing () {
     return ( 
-     <div className="container mt-5">
-      <div className="row">
-        <div className="col-4 mr-5">
+     <div className="container mt-5  p-5">
+      <div className="row ">
+        <div className="col-6 mr-5">
           <h1 className='fs-2'> Unbeatable pricing</h1>
-          <p>We pioneered the concept of discount broking and price 
+          <p>We pioneered the concept of discount broking and price <br />
           transparency in India.Flat fees and no hidden charges.
           </p>
           <a href="" style={{textDecoration:"none"}}>See Pricing<i class="fa-solid fa-arrow-right-long"></i></a>
         </div>
-<div className="col-2"></div>
-        <div className="col-6 ">
+
+        <div className="col-6 mb-5 p-3">
            <div className="row text-center">
               <div className="col border  p-5">
                  <h1 className='mb-4'><i class="fa-solid fa-indian-rupee-sign"></i>0</h1>
