0bde923c40947e3b9b95fa9febee4a8c12b2c0b0
KUNAL CHOURE <chourekunal4@gmail.com>
2024-09-20 19:27:32 +0530

    navbar  updated

--- Frontend/src/landing_page/Navbar.jsx
+++ Frontend/src/landing_page/Navbar.jsx
@@ -3,6 +3,60 @@ import React from 'react'
 
 export default function Navbar() {
     return ( 
-   <h1>Navbar</h1>
+      <nav className="navbar navbar-expand-lg " style={{backgroundClip:"#ffff"}}>
+      <div className="container p-2">
+        <a className="navbar-brand ml-5" href="#">
+          <img src="media/images/logo.svg" alt=""  style={{width:"25%"}}/>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Signup
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                className="nav-link active"
+                href="#"
+                
+              >
+                Products
+              </a>
+              </li>
+                <li>
+                 <a className="nav-link active" href="">
+                   Pricing
+                 </a>
+                </li>
+        
+           
+            <li className="nav-item">
+              <a className="nav-link active " >
+                Support 
+              </a>
+            </li>
+          </ul>
+        
+        </div>
+      </div>
+    </nav>
+  
      );
 }
--- Frontend/src/landing_page/Openaccount.jsx
+++ Frontend/src/landing_page/Openaccount.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function Openaccount(){
     return (
-       <h1>Open Account</h1>
+        <div className='container p-5'>
+        <div className="row text-center">
+         
+          <h1 className='mt-5 '>Open a Zerodha account</h1>
+          <p className='mt-3'>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.
+
+</p>
+        <button className='p-2 btn btn-primary fs-5 mb-5 mt-3'  style={{width:"20%",margin:"0 auto" }}>Sign up for free </button>
+        </div>
+    
+    
+    
+      </div>
     );
 }
