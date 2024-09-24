51fd080c146088ab468562876ca2f31148979b53
KUNAL CHOURE <chourekunal4@gmail.com>
2024-09-24 20:30:02 +0530

    updated

--- Frontend/src/landing_page/about/aboutpage.jsx
+++ Frontend/src/landing_page/about/aboutpage.jsx
@@ -1,10 +1,13 @@
 import { dark } from "@mui/material/styles/createPalette";
 import React from "react";
 import Hero from "./hero";
+import Team from "./team";
 
 export default function Aboutus(){
      return (
+        <>
         <Hero/>
-        
+        <Team/>
+        </>
      )
 }
