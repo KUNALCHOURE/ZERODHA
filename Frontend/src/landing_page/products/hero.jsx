54fbb86cff5cb2d9a394ae3689577242ef132fd3
KUNAL CHOURE <chourekunal4@gmail.com>
2024-09-25 19:29:47 +0530

    Product updated

--- Frontend/src/landing_page/products/hero.jsx
+++ Frontend/src/landing_page/products/hero.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 export default function Hero(){
     return (
-        <div className="container">
-            <div className=" text-center mt-5 ">
-                <h1 className="fs-1">Technology</h1>
-                <p className="fs-4 mt-3 text-muted">Sleek,modern and intutive trading platforms</p>
+        <div className="container mb-5">
+            <div className=" text-center mt-5 p-5 ">
+                <h1 className="text-muted"  style={{fontSize:"50px"}}>Technology</h1>
+                <p className="fs-5 mt-3 text-muted">Sleek,modern and intutive trading platforms</p>
                 <p className="mt-3">Check out out <a href="" style={{textDecoration:"none"}}>investment offerings <i class="fa-solid fa-arrow-right"></i></a></p>
             </div>
-        </div>
+
+            <div className="mt-5 bordert-top">
+                <hr />
+            </div>
+                 </div>
+
+
     )
 
 }
--- Frontend/src/landing_page/products/product.jsx
+++ Frontend/src/landing_page/products/product.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import Hero from "./hero";
+import Leftsection from "./leftsection";
 
 export default function Product(){
     return(
+        <>
         <Hero/>
+         <Leftsection imgurl="media/images/kite.png" producname="Kite" proddesc="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices." trydemo="/dasgboard" learnmore="/learnmore"googleplay="media/images/googleplayBadge.svg"appstore="appstoreBadge.svg"/>
+         </>
     )
 }
