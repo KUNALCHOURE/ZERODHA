8dd592ea6fb20557722032311d6fc0fda8530fba
KUNAL CHOURE <chourekunal4@gmail.com>
2024-10-04 22:57:03 +0530

    updated

--- Frontend/src/landing_page/pricing/brokerage.jsx
+++ Frontend/src/landing_page/pricing/brokerage.jsx
@@ -8,15 +8,15 @@ export default function Brokerage(){
                     <a href="" className="text-decoration-none m-5 fs-2" style={{fontSize:"larger"}}>Brokerage Calculator</a>
 
                     <ul className="mt-5">
-                        <li>Call & Trade and RMS auto-Squareoff :Additional charges of 50+GST per order.</li>
+                        <li>Call & Trade and RMS auto-Squareoff :Additional charges of ₹50+GST per order.</li>
                         <li>Digital contract notes will be sent via e-mail</li>
 
                         <li>Physical copies of contract notes ,if required ,shall be charges 20 per contract note.Courier charges apply</li>
 
-                        <li>For NRI account(non-PIS),0.5% or 100 per executed order for equity(whichever is lower)</li>
-                        <li>For NRI account(PIS),0.5% or 200 per executed order for equity(whichever is lower)</li>
+                        <li>For NRI account(non-PIS),0.5% or ₹100 per executed order for equity(whichever is lower)</li>
+                        <li>For NRI account(PIS),0.5% or ₹200 per executed order for equity(whichever is lower)</li>
 
-                        <li>if the account is in debit balance ,any order placed will be charged 40 per executed order instead of 20 per executed order.</li>
+                        <li>if the account is in debit balance ,any order placed will be charged ₹40 per executed order instead of ₹20 per executed order.</li>
 
                     </ul>
                 </div>
