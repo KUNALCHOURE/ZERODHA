1b689517879f9b06f546fe725ffed60cbe4c4414
KUNAL CHOURE <chourekunal4@gmail.com>
2024-10-12 15:06:15 +0530

    postions  updated

--- Dashboard/src/components/Positions.jsx
+++ Dashboard/src/components/Positions.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import { positions } from "../data/data";
 const Positions = () => {
   return (
     <>
-      <h3 className="title">Positions (2)</h3>
+      <h3 className="title">Positions {positions.length}</h3>
 
       <div className="order-table">
         <table>
@@ -16,6 +16,28 @@ const Positions = () => {
             <th>P&L</th>
             <th>Chg.</th>
           </tr>
+
+          <tbody>
+            {positions.map((stock,index)=>{
+        const currval = stock.price * stock.qty;
+        const isProfit = currval - stock.avg * stock.qty >= 0.0;
+        const profitclass = isProfit ? "profit" : "loss";
+        const dayclass = stock.isLoss ? "loss" : "profit";
+              return(
+               <tr>
+                <td>{stock.product}</td>
+                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
+                <td>{stock.avg.toFixed(2)}</td>
+                <td>{stock.price.toFixed(2)}</td>
+                <td className={profitclass}>
+                    {(currval - stock.avg * stock.qty).toFixed(2)}
+                  </td>
+                  <td className={dayclass}>{stock.day}</td>
+               </tr>
+              );
+            })}
+          </tbody>
         </table>
       </div>
     </>
