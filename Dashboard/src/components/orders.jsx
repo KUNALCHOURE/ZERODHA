5a11828c7fdc3503db956f5ab641d3167fe2d208
KUNAL CHOURE <chourekunal4@gmail.com>
2024-10-19 22:08:30 +0530

    orders updated

--- Dashboard/src/components/orders.jsx
+++ Dashboard/src/components/orders.jsx
@@ -1,15 +1,44 @@
-
-import React, { useState } from "react";
+import axios from 'axios'
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Orders() {
-    const[orders,setorders]=useState({name:"",qty:0,price:0,mode:""});
+    const[orders,setorders]=useState([]);
 
-
+    useEffect(()=>{
+    axios.get("http://localhost:3030/allorders").then((res)=>{
+       console.log(res.data);
+        setorders(res.data);
+      }
+      
+    )},[])
   return (
     <div className="orders">
       <div className="no-orders">
-        <p>You haven't placed any orders today</p>
+      {orders.length === 0 ? (
+          <p>You haven't placed any orders today</p>
+        ) : (
+          <div className='order-table'>
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Quantity</th>
+                  <th>Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orders.map((stock, index) => (
+                  <tr key={index}>
+                    <td>{stock.name}</td>
+                    <td>{stock.qty}</td>
+                    <td>{stock.price}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
 
         <Link to={"/"} className="btn">
           Get started
@@ -17,5 +46,4 @@ export default function Orders() {
       </div>
     </div>
   );
-};
-
+}
